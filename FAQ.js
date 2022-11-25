49c09448d93d794f06f10927c1b48cb62603b454
Pyruslupinus <114001150+Pyruslupinus@users.noreply.github.com>
2022-11-25 14:01:51 +0100

    Test dölja/visa svar

--- FAQ.js
+++ FAQ.js
@@ -1,3 +1,4 @@
+/*Sökfuntktion, jämför bara input string med text i frågor direkt. Inga taggar etc (än) */
 function searchQA() {
     let input = document.getElementById("searchbar").value.toLowerCase();
 
@@ -10,5 +11,44 @@ function searchQA() {
             searchElement[i].style.display = "none";
         }
     }
+}
+ /*Kaos,  Utökning av text*/
+function answer1(){
+    let textToShow = document.getElementById("answer1");
+    let buttonText = document.getElementById("text-button1");
+    toggleText(textToShow, buttonText);
+}
+function answer2(){
+    let textToShow = document.getElementById("answer2");
+    let buttonText = document.getElementById("text-button2");
+    toggleText(textToShow, buttonText);
+}
+function answer3(){
+    let textToShow = document.getElementById("answer3");
+    let buttonText = document.getElementById("text-button3");
+    toggleText(textToShow, buttonText);
+}
 
+function answer4() {
+    let textToShow = document.getElementById("answer4");
+    let buttonText = document.getElementById("guestion-button")
+    if (textToShow.style.display === "block") {
+        textToShow.style.display = "none";
+    } else {
+        textToShow.style.display = "block";
+        //buttonText.innerHTML = "<i class="fa-solid fa-chevron-up"></i>"
+    }
+}
+
+function toggleText(textToShow, buttonText) {
+
+    if (textToShow.style.display === "block") {
+        textToShow.style.display = "none";
+
+        buttonText.innerHTML="Visa svar";
+    } else {
+        textToShow.style.display = "block";
+
+        buttonText.innerHTML="Dölj svar";
+    }
 }
