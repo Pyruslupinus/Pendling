cffd7d963ec92e804c5116edc0400b850bf11ecd
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-25 19:15:02 +0100

    Added icons & gradients to the cards

--- resor/display-trips/TripCard.js
+++ resor/display-trips/TripCard.js
@@ -2,20 +2,26 @@
 //Different cards for Car Owner / Passenger (Icons, Title)
 //Different Cards for Single Trip / Pendling (Color, Subtitle?)
 
-//From - To listed
-//Time listed
+//Some different logo idées from font awesome  - fa-car-side, fa-person-walking fa-child-reaching
 
 const TripCard = (props) => {
   const cardInfo = props.info;
 
   //Conditional operator - testar bool ? om bool = true : om bool = false
-  const cardStyle = {
-    border:
-      "2px solid " + (props.info.traveltype === "resa" ? "#22577a" : "#80ed99"),
-  };
+
+  const borderStyle = getGradientBorder(
+    props.info.traveltype === "resa" ? "#22577a" : "#80ed99"
+  );
 
   return (
-    <article className="trip-card" style={cardStyle}>
+    <article className="trip-card" style={borderStyle}>
+      <i
+        className={`fa-solid ${
+          props.info.type === "passagerare"
+            ? "fa-person-walking-luggage"
+            : "fa-car"
+        } fa-lg`}
+      ></i>
       <p>{cardInfo.type}</p>
       <p>{cardInfo.traveltype}</p>
       <p>{cardInfo.from}</p>
@@ -25,3 +31,10 @@ const TripCard = (props) => {
     </article>
   );
 };
+
+function getGradientBorder(color) {
+  return {
+    border: "2px solid grey",
+    background: `linear-gradient(-10deg, transparent 77%, ${color})`,
+  };
+}
