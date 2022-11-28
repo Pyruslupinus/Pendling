cd66b78c300258a78393236c4b60e9109c911fb4
Pyruslupinus <114001150+Pyruslupinus@users.noreply.github.com>
2022-11-28 09:30:46 +0100

    Visa/dölj frågor m react. Sök trasig

--- FAQ.js
+++ FAQ.js
@@ -12,43 +12,97 @@ function searchQA() {
         }
     }
 }
- /*Kaos,  Utökning av text*/
-function answer1(){
-    let textToShow = document.getElementById("answer1");
-    let buttonText = document.getElementById("text-button1");
-    toggleText(textToShow, buttonText);
-}
-function answer2(){
-    let textToShow = document.getElementById("answer2");
-    let buttonText = document.getElementById("text-button2");
-    toggleText(textToShow, buttonText);
-}
-function answer3(){
-    let textToShow = document.getElementById("answer3");
-    let buttonText = document.getElementById("text-button3");
-    toggleText(textToShow, buttonText);
-}
 
-function answer4() {
-    let textToShow = document.getElementById("answer4");
-    let buttonText = document.getElementById("guestion-button")
-    if (textToShow.style.display === "block") {
-        textToShow.style.display = "none";
-    } else {
-        textToShow.style.display = "block";
-        //buttonText.innerHTML = "<i class="fa-solid fa-chevron-up"></i>"
+/*Visa och dölj svar på frågor*/
+const ContentShowOrHide = (props) => {
+    let question = props.question
+    //let arrow =<i className="fa-solid fa-chevron-down"></i>
+
+    const [arrow, setCurrentArrow] = React.useState(<i className="fa-solid fa-chevron-down"></i>)
+
+    const [answer, setCurrentAnswer] = React.useState("")
+    const ShowOrHideAnswer = () => {
+        if (answer ===""){
+            setCurrentAnswer(props.answer);
+            setCurrentArrow(<i class="fa-solid fa-chevron-up"></i>);
+        } else {
+            setCurrentAnswer("");
+            setCurrentArrow(<i className="fa-solid fa-chevron-down"></i>)
+        }
+        
     }
+    return (
+        <article className="FAQ">
+          <button className="question" onClick={ShowOrHideAnswer}><h2>{arrow} {question}</h2></button>
+          <p className="answer">{answer}</p>
+        </article>
+    )
 }
 
-function toggleText(textToShow, buttonText) {
+const reactContentRoot = document.getElementById("root");
+const App = () => {
+    return (
+        <div>
+            <ContentShowOrHide 
+            question={"Får jag köra min EPA-traktor?"} 
+            answer={" Nej, vi tillåter enbart fordon registerade som bil. Alla förare måste vara över 18 år."} />
+            
+            <ContentShowOrHide 
+            question={"Får jag köra min mopedbil?"}
+            answer={"Nej, vi tillåter enbart fordon registerade som bil. Alla förare måste vara över 18 år."}/>
+            
+            <ContentShowOrHide
+            question={" Jag kör motorcykel till jobbet, kan jag registera mig som förare?"}
+            answer={" Nej, vi tillåter enbart fordon registerade som bil."} />
+             
+             <ContentShowOrHide
+            question={"Kan min 12åring åka själv med en förare jag hittar via pendling.se?"}
+            answer={" Nej, alla passagerare under 18 år måste ha sällskap av en ansvarig vuxen. Passagerare och förare är båda ansvariga för att barn under 135cm har bilbarnstol. Förare har rätt att neka passagerare om sådan säkerhetsutrusting saknas."} />
+             
+             <ContentShowOrHide
+            question={"Får jag neka någon att åka med om de inte använder bälte?"}
+            answer={"Förare är ansvariga för att alla passagere under 16 år använder bilbälte (svensk lag). Om passagerare under 16 år inte använder bälte har föraren rätt att neka passageraren. Föraren får även neka andra passagerare om dessa äventyrar säkerheten för förare eller passagerare. Passagere som nekas att fortsätta resan är inte skyldiga att betala överenskommen ersättning för resten av resan."} />
+             
+             <ContentShowOrHide
+            question={" Vem är ansvarig för att det finns en bilbarnstol till mitt barn?"}
+            answer={" Myndig person som medföljer barnet är ansvarig för att det finns lämplig bilbarnstol. Föraren har rätt att neka passagerare om lämplig bilbarnstol saknas för något barn under 135cm."} />
+             
+             <ContentShowOrHide
+            question={"Tillhandahåller pendling.se någon försäkring?"}
+            answer={" Nej, alla åkning sker på eget ansvar. Observera att bilar i Sverige måste vara minst trafikförsäkrade för att få köras."} />
+             
+             <ContentShowOrHide
+            question={"Hur vet jag att bilen jag åker med är försäkrad?"}
+            answer={"Fråga föraren. För att en bil ska får köras i Sverige måste den vara minst trafikförsäkrad."} />
+             
+             <ContentShowOrHide
+            question={"Kan jag registera mig som både passagerare och bilägare?"}
+            answer={"Ja, vi skiljer inte på passagerarkonton och bilägarkonton. Du kan dock bara registera dig som bilägare eller passagerare på enskilda resor."} />
+             
+             <ContentShowOrHide
+            question={"Får jag dricka alkohol innan jag skjutsar någon?"}
+            answer={"Nej, att köra bil alkoholpåverkad är ollagligt under lagen om rattfylleri, såväl som farligt."} />
 
-    if (textToShow.style.display === "block") {
-        textToShow.style.display = "none";
-
-        buttonText.innerHTML="Visa svar";
-    } else {
-        textToShow.style.display = "block";
-
-        buttonText.innerHTML="Dölj svar";
-    }
+            <ContentShowOrHide
+            question={"Får jag neka någon som är alkholpåverkad från att åka med?"}
+            answer={"Förare har rätt att neka passagere som påverkar säkerheten negativt eller på annat sätt stör ordningen. Passagerare som nekas är inte skyldiga att betala överenskommen ersättning för resten av resan."} />
+            
+            <ContentShowOrHide
+            question={"Behöver jag ha körkort för att registera mig?"}
+            answer={" Du behöver inte ha körtkort för att registera dig, men för att vara förare/bilägare under en resa krävs givetvis körkort."} />
+            
+            <ContentShowOrHide
+            question={"Varför måste jag ha körkort för att vara bilägare?"}
+            answer={"Vi tillåter inte förare utan körkort."} />
+            
+            <ContentShowOrHide
+            question={"Måste jag ha B-körkort?"}
+            answer={" För att skapa resor som bilägare krävs att du har minst B-körkort, då vi enbart tillåter fordon registerade som bil. För att åka som passagerare krävs inget körkort."} />
+            
+      </div>
+    )
 }
+ReactDOM.render(<App />, reactContentRoot);
+
+
+
