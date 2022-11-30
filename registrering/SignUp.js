7f9786c419c097e623d3b658cd8b564968d86731
DanielM <danielmattsson2010hotmail.com>
2022-11-30 21:34:13 +0100

    30/11 ShowPassword & ComparePassword

--- registrering/SignUp.js
+++ registrering/SignUp.js
@@ -1,18 +1,32 @@
 function ShowPasswordFunction() {
   var x = document.getElementById("PasswordInput")
   var i = document.getElementById("PasswordInput2")
-  if (x.type && i.type === "password") {
+  if (x.type === "password") {
     x.type = "text"
-  } else {
+  }
+  if(i.type === "password")
+  {
+    i.type = "text"
+  }
+    else {
     x.type = "password"
+    i.type = "password"
   }
 }
+
+
 
 function RegisterSave() {
   //Sparade inputs variabler
   var EmailInput = document.getElementById("EmailInput").value
   var UserNameInput = document.getElementById("UserNameInput").value
   var PasswordInput = document.getElementById("PasswordInput").value
+  var Password2 = document.getElementById("PasswordInput2").value
+
+  if(PasswordInput != Password2)
+  {
+    alert("Lösenorden måste vara likadana")
+  }
 
   const newUser = {
     //Skapar ett objekt som jag vill ska innehålla email,username och password
@@ -21,8 +35,11 @@ function RegisterSave() {
     password: PasswordInput,
   }
 
+  
+
+
   const userString = localStorage.getItem("Users") //Hämtar 'Users' från localStorage - är just nu i string-format
-  const userArray = JSON.parse(userString) //'parse' på userString från string tillbaka till en array
+  let userArray = JSON.parse(userString) //'parse' på userString från string tillbaka till en array
 
   //Om arrayen nu är null, betyder det att den inte fanns sparad, skapar då en tom array
   if (userArray === null) {
@@ -34,12 +51,14 @@ function RegisterSave() {
 
   //Och konverterar tillbaka till en sträng och skriver över den existerande i localStorage
   localStorage.setItem("Users", JSON.stringify(userArray))
-}
-function TillLogin() {
-  window.location.href = "login.html"
+
+
+
 }
 
+
 function okLogin() {
+
   var savedEmail = localStorage.getItem("EmailInput")
   var savedUsername = localStorage.getItem("UserNameInput")
   var savedPassword = localStorage.getItem("PasswordInput")
@@ -49,4 +68,6 @@ function okLogin() {
   } else {
     alert("Fel")
   }
+
+  
 }
