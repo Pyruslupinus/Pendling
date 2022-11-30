18ff41433a2f678271a0fbb9212f6f7b9f41ce9e
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2022-11-30 20:26:15 +0100

    Improved responsiveness, progress on hamburger.

--- indexjs.js
+++ indexjs.js
@@ -7,8 +7,19 @@ JS to-do list:
 - - - - - - - -
 */
 
-//Knapp-funktion för att komma tillbaka till börjav av sidan.
+//Button at the bottom of the page to get back to the top.
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+//Hamburger menu function do display the option to create account and log in.
+function flipBurger() {
+  var x = document.getElementById("log");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
