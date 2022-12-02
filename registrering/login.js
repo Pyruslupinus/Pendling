10723afcb36847eff4f71cfcaa9927f4f7a26948
MortezaMoppe <morteza.gholami.syne22lin@edu.tucsweden.se>
2022-12-02 13:19:54 +0100

    Test

--- registrering/login.js
+++ registrering/login.js
@@ -4,34 +4,68 @@ const username = document.getElementById('username');
 const userpassword = document.getElementById('userpassword');
 
 
+window.onload = () =>
+{
+    //console.log(sessionStorage.getItem("LoggedIn"));
 
+    if(sessionStorage.getItem("LoggedIn") == "true")
+    {
+        //för att se om personen är inloggade: bara inloggade användare kan skapa resa, se skapa resa knappen.
+        //när man trycker på skapa resa eller sök resa ska den referera till logga in sidan.
+        //ingen användare->registrera sidan
+
+        document.getElementById("inlogg").innerText="Logga Ut"
+    }
+    else{
+       // document.getElementById("inlogg").innerText="Logga In"
+        
+    }
+}
+
+function InLoggad(){
+    sessionStorage.setItem("LoggedIn", true) 
+}
+
+function UtLoggad(){
+    sessionStorage.setItem("LoggedIn", false)
+    onload()
+}
+
+
+
+//oklogin kollar om user är registrerad och att angivna lösenord och username stämmer.
 function okLogin() {
-
+    const registered_users = JSON.parse(localStorage.getItem('Users'));
     
-    /* var savedUsername = localStorage.getItem("UserNameInput")
-     var savedPassword = localStorage.getItem("PasswordInput")
-     */
-     var Users = JSON.parse(localStorage.getItem('Users'));
-     var UserName = document.getElementById("username").value
-     var UserPassword = document.getElementById("userpassword").value
-    
- 
- 
- for (let index = 0; index < Users.length; index++) {
-     const element = Users[index];
- 
-     if (UserName == element.username && UserPassword == element.password) {
-         alert("OK")
-       } else {
-         alert("Fel")
-       }   
- }
-   }
+    var UserName = document.getElementById("username").value
+    var UserPassword = document.getElementById("userpassword").value
 
+    for (user in registered_users) {
+
+       
+        if (UserName == registered_users[user].username && UserPassword == registered_users[user].password) {
+            
+         
+           GoToHomepage()
+           
+           InLoggad()
+         
+        }
+        else if (UserName == '' && UserPassword == '')
+        {
+            //Det finns inget konto med det angivna användarnamnet eller lösenordet.
+            alert("Det finns inget konto med det angivna användarnamnet eller lösenordet.")
+        }
+    }
+}
+
+
+/*
 form.addEventListener('submit', e => {
     e.preventDefault();
-
-    validateInputs();
+    
+    okLogin()
+    //validateInputs();
 });
 
 const setError = (element, message) => {
@@ -52,34 +86,39 @@ const setSuccess = element => {
 };
 
 
+
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const userpasswordValue = userpassword.value.trim();
 
     /*if(Users){
     console.log(Users.username);
-    }*/
+    }
 
     if(usernameValue === ''){
-        setError(username, 'Skriv ditt användarnamn');
-    } /*else if (!Users(usernameValue)){
+       setError(username, 'Skriv ditt användarnamn');
+   } /*else if (!Users(usernameValue)){
         setError(username, 'användarnamn finns inte');
-    }*/ else{
+    } else{
         setSuccess(username);
     }
     if(userpasswordValue === ''){
         setError(userpassword, 'Skriv ditt lösenord');
     } /*else if (!Users(userpasswordValue)){
         setError(userpassword, 'fel lösenord');
-    } */else {
+    } else {
         setSuccess(userpassword);
     }
 };
-   /* if (usernameValue == savedUsername && userpasswordValue == savedPassword) {
-      alert("OK")
-    } else {
-      alert("Fel")
+  
+*/
+
+
+    function GoToHomepage()
+    {
+       window.location.href="../index.html";
+       //alert("gotohomepage")
     }
-    */
 
 
+   
