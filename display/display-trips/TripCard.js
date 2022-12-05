5b5071d5370d40fab124d61cdb1ba1b77940fa16
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-12-05 14:47:54 +0100

    Matched background

--- display/display-trips/TripCard.js
+++ display/display-trips/TripCard.js
@@ -1,5 +1,4 @@
 //Doing everything in one js file to make it a bit less cluttered in the project
-
 //Some different logo ideas from font awesome  - fa-car-side, fa-person-walking fa-child-reaching
 
 const TripCard = (props) => {
@@ -21,18 +20,9 @@ const TripCard = (props) => {
     if ("cardClick" in props) {
       props.cardClick(cardInfo);
     }
-
-    // const loginStatus = sessionStorage.getItem("LoggedIn");
-    // if(loginStatus != "true")
-    // {
-    //   alert("Can't click card when you're not logged in!")
-    // }
-    // else{
-    //   alert("A card was clicked!")
-    // }
-    //console.log(sessionStorage.getItem("LoggedIn"));
   };
-  //Return the actual html
+  
+
   return (
     <article className="trip-card" style={cardStyle} onClick={handleClick}>
       <i
@@ -88,104 +78,6 @@ const TripModal = (props) => {
     </section>
   );
 };
-
-//Conditional operator (tertiary operator) - testar (bool ? om bool = true körs den här sidan : om bool = false körs den här sidan)
-//Går att göra ganska mycket roligt med react och den eftersom vi kan peta in javascript mellan {} i html-stycken
-//Eftersom vi ska ha ut en hel del varierade saker kör jag allt på samma gång däremot
-function getVariableInfo(cardInfo) {
-  return {
-    typeLogo:
-      cardInfo.type === "passagerare" ? "fa-person-walking-luggage" : "fa-car",
-    gradientColor: cardInfo.traveltype === "resa" ? "#22577a" : "#80ed99",
-    dropColor: cardInfo.traveltype === "resa" ? "#22577a" : "#80ed99",
-    traveltypeText:
-      (cardInfo.type === "passagerare" ? "Söker " : "Erbjuder ") +
-      (cardInfo.traveltype === "resa" ? "Resa" : "Pendling"),
-  };
-
-  //Tldr for above is that we're doing some style and text changes based on the 4 possible
-  //combinations of passagerare, ägare, pendling & resa. A more readable method would have been a switch
-  //and functions returning different objects, although that would have made the code much longer
-}
-
-//Originally I did this with the border-image trick from https://youtu.be/ypstT5UfCsk 9 minutes in
-//Now this is a much simpler way of doing it so no more sillyness
-function setGradientBorder(style, color) {
-  style.background = `linear-gradient(135deg, #f0f8ffd0 77%, ${color})`;
-  style.borderColor = `${color}`;
-
-  return style;
-}
-
-function setDropShadow(style, color) {
-  style.boxShadow = `0px 2px ${color}`;
-  return style;
-}
-
-function getTravelDate(dateString) {
-  const travelDate = new Date(dateString);
-
-  return (
-    getDayText(travelDate.getDay()) +
-    ", den " +
-    travelDate.getDate() +
-    " " +
-    getMonthText(travelDate.getMonth())
-  );
-}
-
-//Gonna move these (together with saving & loading from localstorage) to a helper script later
-function getDayText(weekDayIndex) {
-  switch (weekDayIndex) {
-    case 0:
-      return "Måndag";
-    case 1:
-      return "Tisdag";
-    case 2:
-      return "Onsdag";
-    case 3:
-      return "Torsdag";
-    case 4:
-      return "Fredag";
-    case 5:
-      return "Lördag";
-    case 6:
-      return "Söndag";
-    default:
-      return "";
-  }
-}
-
-function getMonthText(monthIndex) {
-  switch (monthIndex) {
-    case 0:
-      return "Januari";
-    case 1:
-      return "Februari";
-    case 2:
-      return "Mars";
-    case 3:
-      return "April";
-    case 4:
-      return "Maj";
-    case 5:
-      return "Juni";
-    case 6:
-      return "Juli";
-    case 7:
-      return "Augusti";
-    case 8:
-      return "September";
-    case 9:
-      return "Oktober";
-    case 10:
-      return "November";
-    case 11:
-      return "Juli";
-    default:
-      return "";
-  }
-}
 
 //I Split these into their own components to make things more readable / scaleable
 const CardTimeInfo = (props) => {
@@ -358,3 +250,105 @@ const CardContactButton = (props) => {
     </button>
   );
 };
+
+
+
+// HELPER METHODS //
+
+//Conditional operator (tertiary operator) - testar (bool ? om bool = true körs den här sidan : om bool = false körs den här sidan)
+//Går att göra ganska mycket roligt med react och den eftersom vi kan peta in javascript mellan {} i html-stycken
+//Eftersom vi ska ha ut en hel del varierade saker kör jag allt på samma gång däremot
+function getVariableInfo(cardInfo) {
+  return {
+    typeLogo:
+      cardInfo.type === "passagerare" ? "fa-person-walking-luggage" : "fa-car",
+    gradientColor: cardInfo.traveltype === "resa" ? "#22577a" : "#80ed99",
+    dropColor: cardInfo.traveltype === "resa" ? "#22577a" : "#80ed99",
+    traveltypeText:
+      (cardInfo.type === "passagerare" ? "Söker " : "Erbjuder ") +
+      (cardInfo.traveltype === "resa" ? "Resa" : "Pendling"),
+  };
+
+  //Tldr for above is that we're doing some style and text changes based on the 4 possible
+  //combinations of passagerare, ägare, pendling & resa. A more readable method would have been a switch
+  //and functions returning different objects, although that would have made the code much longer
+}
+
+//Originally I did this with the border-image trick from https://youtu.be/ypstT5UfCsk 9 minutes in
+//Now this is a much simpler way of doing it so no more sillyness
+function setGradientBorder(style, color) {
+  style.background = `linear-gradient(135deg, #f0f8ffd0 77%, ${color})`;
+  style.borderColor = `${color}`;
+
+  return style;
+}
+
+function setDropShadow(style, color) {
+  style.boxShadow = `0px 2px ${color}`;
+  return style;
+}
+
+function getTravelDate(dateString) {
+  const travelDate = new Date(dateString);
+
+  return (
+    getDayText(travelDate.getDay()) +
+    ", den " +
+    travelDate.getDate() +
+    " " +
+    getMonthText(travelDate.getMonth())
+  );
+}
+
+//Gonna move these (together with saving & loading from localstorage) to a helper script later
+function getDayText(weekDayIndex) {
+  switch (weekDayIndex) {
+    case 0:
+      return "Måndag";
+    case 1:
+      return "Tisdag";
+    case 2:
+      return "Onsdag";
+    case 3:
+      return "Torsdag";
+    case 4:
+      return "Fredag";
+    case 5:
+      return "Lördag";
+    case 6:
+      return "Söndag";
+    default:
+      return "";
+  }
+}
+
+function getMonthText(monthIndex) {
+  switch (monthIndex) {
+    case 0:
+      return "Januari";
+    case 1:
+      return "Februari";
+    case 2:
+      return "Mars";
+    case 3:
+      return "April";
+    case 4:
+      return "Maj";
+    case 5:
+      return "Juni";
+    case 6:
+      return "Juli";
+    case 7:
+      return "Augusti";
+    case 8:
+      return "September";
+    case 9:
+      return "Oktober";
+    case 10:
+      return "November";
+    case 11:
+      return "Juli";
+    default:
+      return "";
+  }
+}
